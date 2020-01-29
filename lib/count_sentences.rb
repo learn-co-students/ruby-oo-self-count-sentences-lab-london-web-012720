9c95dfa34720b96e4e1c0b158215f47a99ffc4da
Amy Franz <amy.franz@gmail.com>
2020-01-29 17:20:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    sentence = self.split(/\.|\?|!/).uniq
+    length = sentence.length
+    length -= 1 if sentence.include? ''
+    length
   end
 end
