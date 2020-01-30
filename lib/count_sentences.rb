730405105a09c0d3c34da05687a8308ead279d4d
alexandre jamarco <alexandre.j@gmail.com>
2020-01-30 10:40:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.tr("!?",".").split(".").reject{|s| s.empty?}.count
   end
 end
+
+#"This is a string! It has three sentences. Right?".count_sentences
+#binding.pry
+
+#"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
